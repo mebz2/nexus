09f06feabf3f2d8553e5db711334b89cf63ecdad
mebz <zelalemmebea@gmail.com>
2025-11-02 17:49:39 +0300

    recreate vite app, installed tailwind

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,36 +1,5 @@
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
-import "./App.css";
-
-function Home() {
-  return <h1>Homepage</h1>;
-}
-
-function About() {
-  return <h1>About page</h1>;
-}
-
-function Contact() {
-  return <h1>Contacts Page</h1>;
-}
-
 function App() {
-  return (
-    <BrowserRouter>
-      <nav>
-        <Link to="/home" className="font-bold">
-          Home
-        </Link>{" "}
-        | <Link to="/home/about">About</Link> |{" "}
-        <Link to="/contact">Contact</Link>
-      </nav>
-
-      <Routes>
-        <Route path="/home" element={<Home />} />
-        <Route path="/home/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <h1>hello</h1>;
 }
 
 export default App;
---
+++ client/src/pages/Login.tsx
@@ -0,0 +1,5 @@
+function Login() {
+  return <h1 className="font-bold text-amber-100 ">Login</h1>;
+}
+
+export default Login;

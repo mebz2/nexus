9460354f7bcc9b4ffadc35c28d58429a83d1de43
mebz <zelalemmebea@gmail.com>
2025-11-10 20:41:14 +0300

    Updated Groups page with mock data.

--- client/src/pages/Groups/Groups.tsx
+++ client/src/pages/Groups/Groups.tsx
@@ -1,25 +1,21 @@
 import { Navbar, GroupCard } from "./components";
 import { SearchBar } from "../../components";
+import MockGroups from "../../mocks/studyGroups.json";
 
 function Groups() {
-  return (
-    <div className=" content-gradient h-screen">
-      <Navbar />
-      <div className="flex flex-col items-center gap-2.5 ">
-        <SearchBar />
-        <div className="h-full w-screen grid grid-cols-4 grid-rows-2 p-10 gap-10">
-          <GroupCard />
-          <GroupCard />
-          <GroupCard />
-          <GroupCard />
-          <GroupCard />
-          <GroupCard />
-          <GroupCard />
-          <GroupCard />
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className=" content-gradient h-screen">
+			<Navbar />
+			<div className="flex flex-col items-center gap-2.5 ">
+				<SearchBar />
+				<div className="h-full w-screen grid grid-cols-4 grid-rows-2 p-10 gap-10">
+					{MockGroups.map((group) => {
+						return <GroupCard key={group.id} {...group} />;
+					})}
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Groups;
--- client/src/pages/Groups/components/GroupCard.tsx
+++ client/src/pages/Groups/components/GroupCard.tsx
@@ -2,34 +2,39 @@ import { useState } from "react";
 import { IoMdArrowRoundForward } from "react-icons/io";
 import { Link } from "react-router-dom";
 
-const GroupCard = () => {
-  const [visible, setVisible] = useState<boolean>(false);
-  return (
-    <Link to="/group/overview">
-      <div
-        onMouseEnter={() => setVisible(true)}
-        onMouseLeave={() => setVisible(false)}
-        className={`
-      h-72 w-96 border border-border p-5 flex
-      flex-col gap-10 card-gradient rounded-xl
-      shadow-lg relative
-      hover:cursor-pointer hover:shadow-xl hover:shadow-[#4C23FF]/20
-      `}
-      >
-        <h2>Calculus Study Group</h2>
-        <p>
-          Weekly calculus problem solving sessions. We focus on derivatives,
-          integrals, and real world applications
-        </p>
-        <IoMdArrowRoundForward
-          className={`text-3xl text-[#1D158C]
-        absolute bottom-2.5 right-3.5
-        ${visible ? "visible" : "invisible"}
-        `}
-        />
-      </div>
-    </Link>
-  );
+type GroupCardProps = {
+	name: string;
+	description: string;
+	members: number;
+	createdAt?: string;
+	updatedAt?: string;
+};
+
+const GroupCard = (group: GroupCardProps) => {
+	const [visible, setVisible] = useState<boolean>(false);
+	return (
+		<Link to="/group/overview">
+			<div
+				onMouseEnter={() => setVisible(true)}
+				onMouseLeave={() => setVisible(false)}
+				className={`
+			      h-72 w-96 border border-border p-5 flex
+			      flex-col gap-10 card-gradient rounded-xl
+			      shadow-lg relative
+			      hover:cursor-pointer hover:shadow-xl hover:shadow-[#4C23FF]/20
+			     `}
+			>
+				<h2>{group.name}</h2>
+				<p>{group.description}</p>
+				<IoMdArrowRoundForward
+					className={`text-3xl text-[#1D158C]
+				        absolute bottom-2.5 right-3.5
+				        ${visible ? "visible" : "invisible"}
+			        `}
+				/>
+			</div>
+		</Link>
+	);
 };
 
 export default GroupCard;

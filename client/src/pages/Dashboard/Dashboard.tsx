f3362c52496291e0f90f99bd2a097cfe6c96293b
mebz <zelalemmebea@gmail.com>
2025-11-10 20:21:39 +0300

    Updated recent activities with mock data.

--- client/src/pages/Dashboard/Dashboard.tsx
+++ client/src/pages/Dashboard/Dashboard.tsx
@@ -1,38 +1,43 @@
 import { useState } from "react";
 import { Activity, Navbar, CardContainer } from "./components";
+import Activities from "../../mocks/activities.json";
 
 export default function Homepage() {
-  const [visible, setVisible] = useState<
-    "group" | "archive" | "calendar" | "inbox" | null
-  >(null);
-  return (
-    <div className="content-gradient h-full">
-      {/*nav bar*/}
-      <Navbar />
-      <div className="flex flex-col  p-20 items-center ">
-        {/*welcome back message*/}
-        <div className=" flex w-[80%]">
-          <h1 className="text-[#6B6875]">Welcome back,</h1>{" "}
-          <h1 className="ml-2"> Name!</h1>
-        </div>
+	const [visible, setVisible] = useState<
+		"group" | "archive" | "calendar" | "inbox" | null
+	>(null);
 
-        {/*cards*/}
-        <CardContainer visible={visible} setVisible={setVisible} />
+	return (
+		<div className="content-gradient h-screen">
+			{/*nav bar*/}
+			<Navbar />
+			<div className="flex flex-col  p-20 items-center ">
+				{/*welcome back message*/}
+				<div className=" flex w-[80%]">
+					<h1 className="text-[#6B6875]">Welcome back,</h1>{" "}
+					<h1 className="ml-2"> Name!</h1>
+				</div>
 
-        {/*recent activities*/}
-        <div className="h-full  w-[80%] rounded-2xl bg-[#EEEDFF] shadow-lg shadow-[#4C23FF]/20 pl-10 pb-2.5 pt-2.5 hover:shadow-2xl">
-          <h1 className=" text-neutral-600">Recent Activities</h1>
-          <p className="text-sm text-[#6B6875] font-medium">
-            Stay updated with your activities
-          </p>
+				{/*cards*/}
+				<CardContainer visible={visible} setVisible={setVisible} />
 
-          {/*Activity*/}
-          <Activity />
-          <Activity />
-          <Activity />
-          <Activity />
-        </div>
-      </div>
-    </div>
-  );
+				{/*recent activities*/}
+				<div className="h-full  w-[80%] rounded-2xl bg-[#EEEDFF] shadow-lg shadow-[#4C23FF]/20 pl-10 pb-2.5 pt-2.5 hover:shadow-2xl">
+					<h1 className=" text-neutral-600">Recent Activities</h1>
+					<p className="text-sm text-[#6B6875] font-medium">
+						Stay updated with your activities
+					</p>
+					{Activities.map((activity) => {
+						return (
+							<Activity
+								key={activity.id}
+								name={activity.name}
+								timestamp={activity.timestamp}
+							/>
+						);
+					})}
+				</div>
+			</div>
+		</div>
+	);
 }
--- client/src/pages/Dashboard/components/Activity.tsx
+++ client/src/pages/Dashboard/components/Activity.tsx
@@ -1,14 +1,18 @@
 import { FaDotCircle } from "react-icons/fa";
-const Activity = () => {
-  return (
-    <div className="flex items-center gap-6 mt-3">
-      <FaDotCircle className="text-purple-800" />
-      <div className="w-full h-14 gap-1 items-end p-2.5 ">
-        <p className="font-semibold">Activity 1</p>
-        <p className="text-xs text-[#737373]">1 day ago</p>
-      </div>
-    </div>
-  );
+type ActivityProps = {
+	name: string;
+	timestamp: number;
+};
+const Activity = ({ name, timestamp }: ActivityProps) => {
+	return (
+		<div className="flex items-center gap-6 mt-3">
+			<FaDotCircle className="text-purple-800" />
+			<div className="w-full h-14 gap-1 items-end p-2.5 ">
+				<p className="font-semibold">{name}</p>
+				<p className="text-xs text-[#737373]">{timestamp}</p>
+			</div>
+		</div>
+	);
 };
 
 export default Activity;

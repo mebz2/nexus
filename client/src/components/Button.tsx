035ebd035e18f1aae5969574980a269c2bd4d072
mebz <zelalemmebea@gmail.com>
2025-11-12 14:49:16 +0300

    Black button

--- client/src/components/Button.tsx
+++ client/src/components/Button.tsx
@@ -1,28 +1,26 @@
 type ButtonProps = {
-  label: string;
-  height: number;
-  width: number;
-  onClick: () => void;
-  radius?: number;
+	label: string;
+	height: number;
+	width: number;
+	onClick: () => void;
+	radius?: number;
 };
 const Button = ({ label, height, width, onClick, radius }: ButtonProps) => {
-  return (
-    <button
-      type="button"
-      style={{ height, width }}
-      onClick={onClick}
-      className={`
-      outline-none
-      text-lg
-      bg-btn text-white ${radius ? `rounded-[${radius}px]` : "rounded-[8px]"} font-medium
-      hover:shadow-lg
-      hover:shadow-btn
-      hover:cursor-pointer
-      `}
-    >
-      {label}
-    </button>
-  );
+	return (
+		<button
+			type="button"
+			style={{ height, width }}
+			onClick={onClick}
+			className={`
+		      outline-none text-lg bg-black text-white
+		      ${radius ? `rounded-[${radius}px]` : "rounded-lg"} font-medium
+		      hover:shadow-xl hover:shadow-black/25
+		      hover:cursor-pointer
+	      `}
+		>
+			{label}
+		</button>
+	);
 };
 
 export default Button;

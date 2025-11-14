ea58b9d64d92432da1dedb8922809984b1cd5d8c
mebz <zelalemmebea@gmail.com>
2025-11-14 11:38:49 +0300

    created a skeleton for the groups and pagination.

--- client/src/pages/Groups/Groups.tsx
+++ client/src/pages/Groups/Groups.tsx
@@ -4,18 +4,20 @@ import { MockGroups } from "../../mocks/";
 function Groups() {
 	return (
 		<Layout>
-			<div className="flex flex-col items-center gap-2.5 h-full overflow-y-auto border">
+			<div className="flex flex-col items-center gap-2.5 h-full  ">
 				<SearchBar />
 				<div
 					className="
-					h-full w-full border grid grid-cols-5 overflow-y-scroll
+					h-full w-full grid grid-cols-5
 					grid-rows-4 pl-15 pr-15 pt-10  pb-10 gap-5 "
 				>
 					{MockGroups.map((group) => {
 						return <GroupCard key={group.id} {...group} />;
 					})}
 
-					<div className="col-span-5  border">pagination</div>
+					<div className="col-span-5 flex justify-center items-center">
+						pagination
+					</div>
 				</div>
 			</div>
 		</Layout>

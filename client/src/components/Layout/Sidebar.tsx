699e9ba30c7f3700cf60b63953759913306271fa
mebz <zelalemmebea@gmail.com>
2025-11-14 23:36:03 +0300

    made elements hoverable and able to redirect.

--- client/src/components/Layout/Sidebar.tsx
+++ client/src/components/Layout/Sidebar.tsx
@@ -1,51 +1,86 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Sidebar = () => {
+	const navigate = useNavigate();
 	return (
 		<div className="w-full bg-white box-border p-2.5 pl-6 border-r border-[#EBEBEB] ">
-			<div className="bg-white w-fit flex mb-2.5">
-				<Link to="/home">
-					<h1 className="m-0 hover:cursor-pointer">nexus</h1>
-				</Link>
+			<div
+				className="bg-white w-fit flex mb-2.5"
+				onClick={() => navigate("/home")}
+			>
+				<h1 className="m-0 hover:cursor-pointer">nexus</h1>
 			</div>
 			<ul className="flex flex-col gap-6">
-				<li>
-					<Link to="/home" className="font-semibold">
-						Home
-					</Link>
+				<li
+					className="text-[#797E87] hover:cursor-pointer hover:text-black font-semibold"
+					onClick={() => navigate("/home")}
+				>
+					Home
 				</li>
-				<li>
-					<Link to="/settings" className="font-semibold">
-						Settings
-					</Link>
+				<li
+					className="text-[#797E87] hover:cursor-pointer hover:text-black font-semibold"
+					onClick={() => navigate("/settings")}
+				>
+					Settings
 				</li>
-				<li className="flex flex-col  gap-3">
-					<Link to="/groups" className="font-semibold">
+				<li className="flex flex-col gap-3 text-[#797E87] hover:cursor-pointer ">
+					<div
+						onClick={() => navigate("/groups")}
+						className="font-semibold w-full hover:text-black"
+					>
 						Groups
-					</Link>
+					</div>
 					<ul className="flex flex-col pl-3 gap-3">
-						<li>Your Groups</li>
-						<li>Public Groups</li>
+						<li
+							className="flex hover:text-black"
+							onClick={() => navigate("/groups")}
+						>
+							Your Groups
+						</li>
+						<li
+							className="flex hover:text-black"
+							onClick={() => navigate("/groups")}
+						>
+							Public Groups
+						</li>
 					</ul>
 				</li>
-				<li>
-					<Link to="/archive" className="font-semibold">
-						Archive
-					</Link>
+				<li
+					className="text-[#797E87] hover:cursor-pointer hover:text-black font-semibold"
+					onClick={() => navigate("/archive")}
+				>
+					Archive
 				</li>
-				<li className="flex flex-col  gap-3">
-					<Link to="/calendar" className="font-semibold">
+				<li
+					onClick={() => navigate("/calendar")}
+					className="flex flex-col text-[#797E87] gap-3 hover:cursor-pointer "
+				>
+					<div
+						className="hover:text-black w-full"
+						onClick={() => navigate("/calendar")}
+					>
 						Calendar
-					</Link>
+					</div>
 					<ul className="flex flex-col pl-3 gap-3">
-						<li>Private</li>
-						<li>Group</li>
+						<li
+							className="flex hover:text-black"
+							onClick={() => navigate("/calendar")}
+						>
+							Private
+						</li>
+						<li
+							onClick={() => navigate("/calendar")}
+							className="flex hover:text-black"
+						>
+							Group
+						</li>
 					</ul>
 				</li>
-				<li>
-					<Link to="/inbox" className="font-semibold">
-						Inbox
-					</Link>
+				<li
+					className="text-[#797E87] hover:cursor-pointer hover:text-black font-semibold "
+					onClick={() => navigate("/inbox")}
+				>
+					Inbox
 				</li>
 			</ul>
 		</div>

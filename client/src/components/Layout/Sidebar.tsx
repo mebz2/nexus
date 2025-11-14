e7fd7064e4440e71c9142774e10083dc9e38fc45
mebz <zelalemmebea@gmail.com>
2025-11-14 11:39:03 +0300

    Experimenting with lists

--- client/src/components/Layout/Sidebar.tsx
+++ client/src/components/Layout/Sidebar.tsx
@@ -1,8 +1,15 @@
 const Sidebar = () => {
 	return (
 		<div className="w-[15%] bg-[#E8E9EB] box-border p-2.5 ">
-			<ul>
-				<li>one</li>
+			<ul className="list-disc pl-6 ">
+				<li>
+					one
+					<ul className="pl-3 list-disc">
+						<li>two</li>
+						<li>two</li>
+						<li>two</li>
+					</ul>
+				</li>
 				<li>one</li>
 				<li>one</li>
 				<li>one</li>

61198b1334290068b2048dfb15df999c915e2087
mebz <zelalemmebea@gmail.com>
2025-11-19 11:42:11 +0300

    Updated export

--- client/src/pages/index.ts
+++ client/src/pages/index.ts
@@ -1,4 +1,4 @@
-export { default as Dashboard } from "./Dashboard/Dashboard";
+export { default as Home } from "./Home/Home";
 export { default as Login } from "./Login/Login";
 export { default as Signup } from "./Signup/Signup";
 export { default as Groups } from "./Groups/Groups";

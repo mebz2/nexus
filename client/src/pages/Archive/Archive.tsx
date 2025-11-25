74627b418d951ead66e75996ea2d1c70e81a63f5
mebz <zelalemmebea@gmail.com>
2025-11-25 17:33:11 +0300

    archive

--- client/src/pages/Archive/Archive.tsx
+++ client/src/pages/Archive/Archive.tsx
@@ -3,8 +3,10 @@ import { Layout } from "../../components";
 function Archive() {
 	return (
 		<Layout>
-			<div className=" p-10 gap-5 h-full flex flex-col items-center">
-				<div className="border rounded-sm h-[8%] w-full">file1</div>
+			<div className="h-full flex flex-col items-center p-10">
+				<div className="w-full h-[8%] border rounded-sm">
+					File
+				</div>
 			</div>
 		</Layout>
 	);

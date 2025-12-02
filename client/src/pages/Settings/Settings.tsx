cbf25750977ace69a4382f62ea64626d947dd0c8
mebz <zelalemmebea@gmail.com>
2025-12-02 07:16:29 +0300

    user information form done

--- client/src/pages/Settings/Settings.tsx
+++ client/src/pages/Settings/Settings.tsx
@@ -1,54 +1,101 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Layout } from "../../components";
 import { MdModeEdit } from "react-icons/md";
 
 function Settings() {
 	const [username, setUsername] = useState<string>("Username");
-	const [edit, setEdit] = useState<boolean>(false)
+	const [edit, setEdit] = useState<boolean>(false);
+	const [save, setSave] = useState<boolean>(false);
 
 	function handleEdit() {
-		setEdit(true);
-		const userInput = document.getElementById("username");
-		if (userInput) {
-			userInput.focus();
-		}
+		setEdit(!edit);
 	}
+
+	function handleSave() {
+		setSave(true);
+		setEdit(false);
+	}
+
+	useEffect(() => {
+		const userInput = document.getElementById("username");
+		const newName = userInput?.textContent;
+		if (save && newName) {
+			setUsername(newName);
+			setEdit(false)
+		}
+	}, [save]);
+
+	useEffect(() => {
+		const userInput = document.getElementById("username");
+		if (edit) {
+			if (userInput) {
+				userInput.focus();
+			}
+		}
+	}, [edit])
+
 
 	return (
 		<Layout>
-			<div className="h-230 overflow-y-auto flex flex-col items-center p-3">
+			<div className="h-230 overflow-y-auto flex flex-col items-center p-3 gap-5">
+				<h2 className="">User Information</h2>
 				<div className="
-					w-5xl h-[280px] rounded-t-2xl border-2 border-border 
+					w-3xl h-[380px] rounded-2xl border-2 border-border 
 					flex flex-col items-center p-5 box-border
 				">
 					<MdModeEdit
 						onClick={handleEdit}
 						className="self-end  text-2xl hover:cursor-pointer"
 					/>
-					<input
-						id="username"
-						type="text"
-						disabled={!edit}
-						placeholder={username}
-						className="border-b-2 flex outline-none"
-					/>
 
-					<div className="w-full mt-auto flex justify-end gap-5">
-						<button
+					<form className="
+						w-[70%] flex flex-col items-center h-70
+						gap-8 
+					"
+					>
+
+						<input
+							id="username"
+							type="text"
+							disabled={!edit}
+							placeholder={username}
+							className="border-b-2 flex outline-none"
+						/>
+
+						<textarea
+							id="bio"
+							placeholder="bio"
+							disabled={!edit}
 							className="
+								outline-none border border-border
+								rounded-sm focus:border-primary
+								min-h-[100px] 
+							"
+						/>
+
+						<div className="w-full mt-auto flex justify-end gap-5">
+							<button
+								onClick={handleSave}
+								className="
 								border-2 p-1 w-20 self-end mt-auto rounded-sm border-border
 								hover:cursor-pointer  hover:shadow-lg outline-none
 							"
-						>
-							Save
-						</button>
-						<button className="
-							border-2 p-1 w-20 self-end mt-auto rounded-sm border-border
-							hover:cursor-pointer  hover:shadow-lg outline-none
-						">
-							Discard
-						</button>
-					</div>
+							>
+								Save
+							</button>
+							<button
+								onClick={handleEdit}
+								type="reset"
+								className="
+								border-2 p-1 w-20 self-end mt-auto rounded-sm border-border
+								hover:cursor-pointer  hover:shadow-lg outline-none
+							"
+							>
+								Discard
+							</button>
+
+						</div>
+					</form>
 
 				</div>
 			</div>

63804b0b1234fb69ff5f6b147e2b7b2e1376dbad
mebz <zelalemmebea@gmail.com>
2026-01-29 11:34:07 +0300

    removed userRouter

--- server/src/server.js
+++ server/src/server.js
@@ -15,14 +15,12 @@ app.get('/', (req, res) => {
 })
 app.listen(3000)
 
-const userRouter = require('./routes/users.js')
 const authRouter = require('./routes/auth.js');
 const groupsRouter = require('./routes/groups.js');
 const filesRouter = require('./routes/files.js');
 const tasksRouter = require('./routes/tasks.js');
 const notificationsRouter = require('./routes/notifications.js'); // Import the new notifications router
 
-app.use('/api/users', userRouter)
 app.use('/api/auth', authRouter);
 app.use('/api/groups', groupsRouter);
 app.use('/api', filesRouter);

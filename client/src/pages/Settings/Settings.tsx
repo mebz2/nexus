0a03406ac84992c0ded1128b489bf2941f2ed69e
mebz <zelalemmebea@gmail.com>
2026-01-30 11:09:43 +0300

    used custom textfield

--- client/src/pages/Settings/Settings.tsx
+++ client/src/pages/Settings/Settings.tsx
@@ -1,7 +1,5 @@
-// import { useEffect, useState } from "react";
-// import { Layout } from "@/components";
 
-import { Layout } from "@/components";
+import { Layout, Textfield } from "@/components";
 
 // import { MdModeEdit } from "react-icons/md";
 function Settings() {
@@ -17,15 +15,17 @@ function Settings() {
 						<h2 className="text-lg font-semibold mb-4">Profile Information</h2>
 						<div className="space-y-4">
 							<div>
-								<label className="block text-sm font-medium text-gray-700 mb-1">Username</label>
+								{/* <label className="block text-sm font-medium text-gray-700 mb-1">Username</label> */}
 								<div className="flex gap-2">
-									<input
+									<Textfield
+										id="username"
+										errorId="username-error"
+										placeholder="username"
+										label="username"
 										type="text"
-										value="username"
-										// onChange={(e) => setUsername(e.target.value)}
-										className="flex-1 p-2 border border-gray-300 rounded-md focus:ring-2 focus:ring-blue-500 outline-none"
 									/>
-									<button className="px-4 py-2 bg-blue-600 text-white rounded-md text-sm hover:bg-blue-700 transition">
+									<button className="px-4 py-2 bg-primary text-white rounded-md text-sm hover:shadow-lg
+									hover:font-bold transition cursor-pointer self-end h-10">
 										Save
 									</button>
 								</div>
@@ -48,13 +48,15 @@ function Settings() {
 						<h2 className="text-lg font-semibold mb-4 text-gray-900">Account Actions</h2>
 						<div className="flex flex-col md:flex-row gap-4">
 							<button
-								className="flex-1 px-4 py-2 bg-gray-100 text-gray-700 rounded-md text-sm font-medium hover:bg-gray-200 transition"
+								className="flex-1 px-4 py-2 bg-gray-100 text-gray-700 rounded-md text-sm font-medium hover:bg-gray-200 
+								cursor-pointer transition"
 								onClick={() => alert("Logging out...")}
 							>
 								Log Out
 							</button>
 							<button
-								className="flex-1 px-4 py-2 bg-red-50 text-red-600 border border-red-200 rounded-md text-sm font-medium hover:bg-red-100 transition"
+								className="flex-1 px-4 py-2 bg-red-50 text-red-600 border border-red-200 rounded-md text-sm font-medium 
+								cursor-pointer hover:bg-red-100 transition"
 								onClick={() => alert("Are you sure you want to delete your account?")}
 							>
 								Delete Account
@@ -68,3 +70,9 @@ function Settings() {
 	);
 }
 export default Settings;
+// <input
+// 										type="text"
+// 										value="username"
+// 										// onChange={(e) => setUsername(e.target.value)}
+// 										className="flex-1 p-2 border border-gray-300 rounded-md focus:ring-2 focus:ring-blue-500 outline-none"
+// 									/>

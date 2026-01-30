004b17ba70f588b5e8cf0fe439b6f0f498fc31da
mebz <zelalemmebea@gmail.com>
2026-01-30 18:04:54 +0300

    database successfully connected

--- server/src/server.js
+++ server/src/server.js
@@ -1,29 +1,39 @@
-const express = require("express")
-const app = express()
-const cors = require('cors')
+require('dotenv').config();
+console.log(`My uri is: ${process.env.MONGO_URI}`);
+const express = require("express");
+const cors = require('cors');
+const connectDB = require('./config/db');
 
-// Middleware for parsing JSON bodies
-app.use(express.json());
+// initialize app
+const app = express();
+
+// connect to database
+connectDB();
 
 // Use CORS middleware to allow requests from the client
 app.use(cors({
-	origin: 'http://localhost:5173' // Adjust port if your client runs on a different one
+	origin: 'http://localhost:5173', // Adjust port if your client runs on a different one
+	credentials: true
 }));
+// Middleware for parsing JSON bodies
+app.use(express.json());
 
-app.get('/', (req, res) => {
-	res.send("hello")
-})
-
+// routes
 const authRouter = require('./routes/auth.js');
 const groupsRouter = require('./routes/groups.js');
 const filesRouter = require('./routes/files.js');
 const notificationsRouter = require('./routes/notifications.js'); // Import the new notifications router
-// const settingsRouter = require('./routes/settings.js');
 
 app.use('/api/auth', authRouter);
 app.use('/api/groups', groupsRouter);
 app.use('/api', filesRouter);
 app.use('/api', notificationsRouter);
-// app.use('/api/settings', settingsRouter);
 
-app.listen(3000)
+app.get('/', (req, res) => {
+	res.send("API is running")
+})
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+	console.log(`Server ready at http://localhost:${PORT}`);
+})

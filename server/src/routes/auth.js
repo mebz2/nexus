daae9ad6eb00492a7d34d1559231ae7fd8914dd0
mebz <zelalemmebea@gmail.com>
2026-01-30 19:15:44 +0300

    made responses json

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -5,21 +5,21 @@ const router = express.Router();
 // @desc    Register user
 // @access  Public
 router.post('/signup', (req, res) => {
-	res.send("Singup route");
+	res.json({ message: "Signup route" });
 });
 
 // @route   POST api/auth/login
 // @desc    Authenticate user & get token
 // @access  Public
 router.post('/login', (req, res) => {
-	res.send("Login route");
+	res.json({ message: "Login route" });
 });
 
 // @route   GET api/auth/me
 // @desc    Get current user
 // @access  Private
 router.get('/me', (req, res) => {
-	res.send("Get messages");
+	res.json({ message: "Get me" });
 });
 
 module.exports = router;
